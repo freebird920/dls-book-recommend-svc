56cc672c2938a3cea1b6f72078040f264a4ed250
퇴역군인 <freebird_han@naver.com>
2025-05-12 15:57:23 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useState } from "react";
+import { Fragment, memo, useCallback, useState } from "react";
 import { GoogleGenAI } from "@google/genai";
 
 const App = memo(() => {
@@ -83,108 +83,114 @@ const App = memo(() => {
     [geminiReq, myApiKey]
   );
   return (
-    <article className="@container space-y-2">
-      <h1>DLS 도서추천 서비스 예시</h1>
-      <section className="border-2 px-5 flex flex-col space-y-2">
-        <h2>APIKEY</h2>
-        <section className="flex space-x-2">
-          <input
-            placeholder="GOOGLE_AI_STUDIO_API_KEY"
-            type="text"
-            name="api-key"
-            form="form-api-key"
-            className="border-2 grow rounded-md"
-          ></input>
-          <button
-            form="form-api-key"
-            className="border-2 px-2 rounded-md"
-            type="submit"
-          >
-            입력
-          </button>
-        </section>
-        <input
-          className="border-2 rounded-md grow"
-          placeholder={"API key 입력하고 입력버튼 누르십시오."}
-          readOnly={true}
-          defaultValue={myApiKey}
-        ></input>
-        <p>
-          APIKEY는{" "}
-          <a
-            href="https://aistudio.google.com/app/apikey"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            링크
-          </a>
-          를 통해 발급받을 수 있습니다.
-        </p>
-      </section>
-
-      <section className="border-2 p-4">
-        <h2>요청</h2>
-        <div className="flex flex-row grow">
-          <label htmlFor="api-req-user-info">userInfo</label>
-          <input
-            name="api-req-user-info"
-            form="form-api-req"
-            className="border-2 grow "
-            placeholder="유저정보(예: 중학교 2학년)"
-          ></input>
-        </div>
-        <div className="flex flex-row grow">
-          <label htmlFor="api-req-user-req">userReq</label>
-          <input
-            name="api-req-user-req"
-            form="form-api-req"
-            className="border-2 grow "
-            placeholder="요청내용(예: 2학기 국어 수행평가용 도서 추천)"
-          ></input>
-        </div>
-        <div className="flex flex-col space-y-2">
-          <div>최근 읽은 도서</div>
-          {Array.from({ length: bookInputCount }).map((_, index) => {
-            return (
+    <Fragment>
+      <div className="@container">
+        <article className="mx-auto space-y-2 max-w-2xl p-4">
+          <h1 className="font-extrabold text-center">
+            DLS 도서추천 서비스 예시
+          </h1>
+          <section className="border-2 px-5 flex flex-col space-y-2">
+            <h2>APIKEY</h2>
+            <section className="flex space-x-2">
               <input
-                className="border-2"
-                key={`${index}-bookinfoInput`}
-                form="form-api-req"
-                name="api-req-book-info"
-                placeholder="서명(저자) 예: 소년이 온다(한강)"
+                placeholder="GOOGLE_AI_STUDIO_API_KEY"
+                type="text"
+                name="api-key"
+                form="form-api-key"
+                className="border-2 grow rounded-md"
               ></input>
-            );
-          })}
-          <button
-            className="border-2"
-            onClick={() => {
-              setBookInputCount(bookInputCount + 1);
-            }}
-          >
-            책 추가++
-          </button>
-        </div>
+              <button
+                form="form-api-key"
+                className="border-2 px-2 rounded-md"
+                type="submit"
+              >
+                입력
+              </button>
+            </section>
+            <input
+              className="border-2 rounded-md grow"
+              placeholder={"API key 입력하고 입력버튼 누르십시오."}
+              readOnly={true}
+              defaultValue={myApiKey}
+            ></input>
+            <p>
+              APIKEY는{" "}
+              <a
+                href="https://aistudio.google.com/app/apikey"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                링크
+              </a>
+              를 통해 발급받을 수 있습니다.
+            </p>
+          </section>
 
-        <button
-          className="rounded-md border-2 grow w-full bg-emerald-500/30 hover:bg-emerald-500/50"
-          type="submit"
-          form="form-api-req"
-        >
-          책 추천 받기
-        </button>
-      </section>
-      <article className="border-2 px-5">
-        <h2>GEMINI답변</h2>
-        <pre className="font-sans whitespace-pre-wrap break-words">
-          {geminiRes.map((res, index) => {
-            return <span key={`${index}-geminiRes`}>{res}</span>;
-          })}
-        </pre>
-      </article>
+          <section className="border-2 p-4">
+            <h2>요청</h2>
+            <div className="flex flex-row grow">
+              <label htmlFor="api-req-user-info">userInfo</label>
+              <input
+                name="api-req-user-info"
+                form="form-api-req"
+                className="border-2 grow "
+                placeholder="유저정보(예: 중학교 2학년)"
+              ></input>
+            </div>
+            <div className="flex flex-row grow">
+              <label htmlFor="api-req-user-req">userReq</label>
+              <input
+                name="api-req-user-req"
+                form="form-api-req"
+                className="border-2 grow "
+                placeholder="요청내용(예: 2학기 국어 수행평가용 도서 추천)"
+              ></input>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <div>최근 읽은 도서</div>
+              {Array.from({ length: bookInputCount }).map((_, index) => {
+                return (
+                  <input
+                    className="border-2"
+                    key={`${index}-bookinfoInput`}
+                    form="form-api-req"
+                    name="api-req-book-info"
+                    placeholder="서명(저자) 예: 소년이 온다(한강)"
+                  ></input>
+                );
+              })}
+              <button
+                className="border-2"
+                onClick={() => {
+                  setBookInputCount(bookInputCount + 1);
+                }}
+              >
+                책 추가++
+              </button>
+            </div>
 
-      <form id="form-api-key" onSubmit={onApiKeyFormSubmit}></form>
-      <form id="form-api-req" onSubmit={onApiReqFormSubmit}></form>
-    </article>
+            <button
+              className="rounded-md border-2 grow w-full bg-emerald-500/30 hover:bg-emerald-500/50"
+              type="submit"
+              form="form-api-req"
+            >
+              책 추천 받기
+            </button>
+          </section>
+          <article className="border-2 px-5">
+            <h2>GEMINI답변</h2>
+            <pre className="font-sans whitespace-pre-wrap break-words">
+              {geminiRes.map((res, index) => {
+                return <span key={`${index}-geminiRes`}>{res}</span>;
+              })}
+            </pre>
+          </article>
+
+          <form id="form-api-key" onSubmit={onApiKeyFormSubmit}></form>
+          <form id="form-api-req" onSubmit={onApiReqFormSubmit}></form>
+        </article>
+      </div>
+    </Fragment>
   );
 });
 export default App;
